17e29196c75de91e0865b333b1f390711933b37f
Rafael Lucini <rafaellucini@gmail.com>
2022-01-14 09:11:00 -0300

    Teste dos pipes

--- src/app/shared/pipes/cpf.pipe.spec.ts
+++ src/app/shared/pipes/cpf.pipe.spec.ts
@@ -5,4 +5,14 @@ describe('CpfPipe', () => {
     const pipe = new CpfPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('if null should return empty', () => {
+    const pipe = new CpfPipe();
+    expect(pipe.transform(undefined)).toBe('');
+  });
+
+  it('should return formatted', () => {
+    const pipe = new CpfPipe();
+    expect(pipe.transform(17667385084)).toBe('176.673.850-84');
+  });
 });
--- src/app/shared/pipes/fone.pipe.spec.ts
+++ src/app/shared/pipes/fone.pipe.spec.ts
@@ -5,4 +5,44 @@ describe('FonePipe', () => {
     const pipe = new FonePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('when length is greater than 12 should return formatted', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform('1234567890123')).toBe('+12 (34) 56789-0123');
+  })
+
+  it('when length is greater than 11 should return formatted', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform('123456789012')).toBe('+12 (34) 5678-9012');
+  })
+
+  it('when length is greater than 10 should return formatted', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform('12345678901')).toBe('(12) 34567-8901');
+  })
+
+  it('when length is greater than 9 should return formatted', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform('1234567891')).toBe('(12) 3456-7891');
+  })
+
+  it('when length is greater than 5 should return formatted', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform('1234567')).toBe('(12) 3456-7');
+  })
+
+  it('when length is greater than 1 should return formatted', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform('12')).toBe('(12) ');
+  })
+
+  it('when length is 1 should return formatted', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform('1')).toBe('(1');
+  })
+
+  it('if null should return empty', () => {
+    const pipe = new FonePipe();
+    expect(pipe.transform(null)).toBe('');
+  })
 });
