4c227344ed44e283032d1b241f95809e4cef4f14
Rafael Lucini <rafaellucini@gmail.com>
2022-01-14 10:18:58 -0300

    Teste do Form e da Lista

--- src/app/page/usuario/usuario-form/usuario-form.component.spec.ts
+++ src/app/page/usuario/usuario-form/usuario-form.component.spec.ts
@@ -1,8 +1,12 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { Usuario } from '../usuario.model';
+import { selectUsuarios } from '../usuario.selectors';
 
 import { UsuarioFormComponent } from './usuario-form.component';
 
@@ -11,17 +15,35 @@ describe('UsuarioFormComponent', () => {
   let fixture: ComponentFixture<UsuarioFormComponent>;
   let store: MockStore;
 
+  const usuario: Usuario = {
+    id: 1,
+    name: 'Usuario 1',
+    email: 'email@email',
+    phone: '123456789',
+    cpf: '17667385084'
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      imports: [ RouterTestingModule, ReactiveFormsModule ],
-      declarations: [ UsuarioFormComponent ],
+      imports: [RouterTestingModule, ReactiveFormsModule],
+      declarations: [UsuarioFormComponent],
       providers: [
         FormBuilder,
-        provideMockStore({ initialState: { loading: false } }),
+        provideMockStore({ initialState: { loading: false, usuarios: [usuario] } }),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(
+              convertToParamMap({
+                id: '1',
+              })
+            )
+          },
+        },
       ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -35,4 +57,27 @@ describe('UsuarioFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create form', () => {
+    expect(component.form).toBeTruthy();
+  });
+
+  it('should edit form', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.form.value).toEqual(usuario);
+  });
+
+  it('should save form', () => {
+    usuario.id = 2;
+    component.form.patchValue(usuario);
+    component.onSubmit();
+    fixture.detectChanges();
+    store.select(selectUsuarios).subscribe(usuarios => {
+      expect(usuarios.length).toBe(2);
+      expect(usuarios[1].id).toEqual(2);
+    });
+  });
+
+
 });
--- src/app/page/usuario/usuario-list/usuario-list.component.spec.ts
+++ src/app/page/usuario/usuario-list/usuario-list.component.spec.ts
@@ -1,24 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { UsuarioService } from '../usuario.service';
 
 import { UsuarioListComponent } from './usuario-list.component';
 import { of } from 'rxjs';
-import { CardComponent } from '@app/shared/components/card/card.component';
+import { Usuario } from '../usuario.model';
+import { SharedModule } from '@app/shared/shared.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppState } from '@app/app.state';
+import { selectUsuarios } from '../usuario.selectors';
 
 describe('UsuarioListComponent', () => {
   let component: UsuarioListComponent;
   let fixture: ComponentFixture<UsuarioListComponent>;
   let service: UsuarioService;
+  let store: MockStore<AppState>;
+
+  const usuarios: ReadonlyArray<Usuario> = [{
+    id: 1,
+    name: 'Usuario 1',
+    email: 'usuario@email.com',
+    phone: '123456789',
+    cpf: '17667385084'
+  }];
+
+  const initialState = {
+    usuarios: usuarios,
+    loading: false,
+  };
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [ UsuarioListComponent, CardComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule, SharedModule],
+      declarations: [ UsuarioListComponent ],
       providers: [
         UsuarioService,
-        provideMockStore({ initialState: { loading: false } }),
+        provideMockStore({ initialState }),
       ]
     })
     .compileComponents();
@@ -30,9 +49,41 @@ describe('UsuarioListComponent', () => {
     fixture.detectChanges();
     service = TestBed.inject(UsuarioService);
     jest.spyOn(service, 'findAll').mockReturnValue(of([]));
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should remove', () => {
+    const id = 1;
+    component.remover(id);
+    fixture.detectChanges();
+    store.select(selectUsuarios).subscribe(usuarios => {
+      expect(usuarios.length).toBe(0);
+    });
+  });
+
+  it('should not remove', () => {
+    const id = 2;
+    component.remover(id);
+    fixture.detectChanges();
+    store.select(selectUsuarios).subscribe(usuarios => {
+      expect(usuarios.length).toBe(1);
+    });
+  });
+
+  it('should call service', () => {
+    component.ngOnInit();
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should return usuarios', () => {
+    jest.spyOn(service, 'findAll').mockReturnValue(of([...usuarios, ...usuarios]));
+    component.ngOnInit();
+    store.select(selectUsuarios).subscribe(usuarios => {
+      expect(usuarios.length).toBe(2);
+    });
   });
 });
