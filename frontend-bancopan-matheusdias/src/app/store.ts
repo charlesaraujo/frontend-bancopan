e733e6b043031ca2f344c94dd40c18ed380e4eda
matheuscara <matheus.dias.dev@gmail.com>
2022-07-20 22:20:02 -0300

    fix/readme src and feature base delete

--- frontend-bancopan-matheusdias/src/app/store.ts
+++ frontend-bancopan-matheusdias/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import usersReducer from "../features/users/usersRedux";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     users: usersReducer
   },
 });
